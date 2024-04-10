dfbd3443d9f2fdb003805e111cecf49bab5d97f7
Cherry Duong <duongcherry@gmail.com>
2024-04-10 12:54:49 -0700

    completed even-numbers-in-an-array

---
+++ 7kyu/even-numbers-in-an-array.js
@@ -0,0 +1,26 @@
+// Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+
+// For example:
+
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+
+//My solution
+
+// function evenNumbers(array, number) {
+
+//   //filter out all even numbers from array
+//   //slice array to be the length of number
+  
+//   let evensArray = array.filter(n => n % 2 === 0)
+//   return newArr = evensArray.slice(-number)
+
+// }
+
+//Refactoring code
+
+function evenNumbers(array,number){
+    return array.filer(n => n % 2 === 0).slice(-number)
+}
+
