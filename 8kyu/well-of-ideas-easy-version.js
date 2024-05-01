6625da6a8b61c082fe6b4466d7c259789e35b4a5
Cherry Duong <duongcherry@gmail.com>
2024-04-30 20:55:56 -0700

    completed 8kyu

---
+++ 8kyu/well-of-ideas-easy-version.js
@@ -0,0 +1,24 @@
+
+// DESCRIPTION:
+// For every good kata idea there seem to be quite a few bad ones!
+
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+
+
+
+function well(x){
+  
+  //filter method and get length of array 
+  let counter = x.filter(ideas => ideas == 'good').length
+  
+  //conditional 
+  if(counter < 1){
+    return 'Fail!'
+  }else if(counter < 3){
+    return 'Publish!'
+  }else if (counter >= 3){
+    return 'I smell a series!'
+  }
+  
+}
