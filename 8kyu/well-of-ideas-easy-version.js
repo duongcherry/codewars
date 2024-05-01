a578ac87251927831adc1ba26de216c8450ac631
Cherry Duong <duongcherry@gmail.com>
2024-04-30 20:56:51 -0700

    added another solution

--- 8kyu/well-of-ideas-easy-version.js
+++ 8kyu/well-of-ideas-easy-version.js
@@ -22,3 +22,22 @@ function well(x){
   }
   
 }
+
+
+//function well(x){
+
+// let counter = 0
+  
+// for(let i = 0; i < x.length; i++){
+//   if(x[i]  == 'good'){
+//     counter++
+//   }
+// }
+
+// if(counter < 1){
+//   return 'Fail!'
+// }else if(counter < 3){
+//   return 'Publish!'
+// }else if (counter >= 3){
+//   return 'I smell a series!'
+// }
