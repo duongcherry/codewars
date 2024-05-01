78be40f09bd94d115158ff2f7180c534096d7eda
Cherry Duong <duongcherry@gmail.com>
2024-05-01 14:05:22 -0700

    completed 8kyu

---
+++ 8kyu/convert-boolean-to-string.js
@@ -0,0 +1,30 @@
+// DESCRIPTION:
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+
+function booleanToString(b){
+    return b.toString()
+  }
+
+
+// function booleanToString(b){
+//     // boolean 
+//     // return a string 
+    
+//     if(b === true){
+//       return "true"
+//     }else if(b === false){
+//       return "false"
+//     }
+    
+//     return b === true ? "true" : "false"
+//   }
+  
+  
+  //true or false, no numbers or special characters 
+  
+  //return "true" "false"
+  
+  //true returns "true" false returns "false"
